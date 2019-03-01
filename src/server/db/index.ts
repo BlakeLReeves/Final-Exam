f2435273797a2f49f04e153f70ca1e882f512df3
blakereeves19 <blake.reeves19@gmail.com>
2019-03-01 12:02:58 -0600

    Created connection to db, and setup server file

---
+++ src/server/db/index.ts
@@ -0,0 +1,20 @@
+import * as mysql from 'mysql';
+import config from '../config';
+import { resolve } from 'url';
+
+
+
+export const pool = mysql.createPool(config.mysql);
+
+export const Query = (query: string, values?: Array<string | number>) => {
+    return new Promise<Array<any>>((resolve, reject) => {
+        pool.query(query, [values], (err, results) => {
+            if(err) reject(err);
+            return resolve(results);
+        });
+    });
+};
+
+export default {
+    
+}
